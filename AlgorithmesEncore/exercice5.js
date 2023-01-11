08168ad5cdf61609f00256d3b9dd7fb05d84e1fe
thimibka <fseetp@gmail.com>
2023-01-11 16:55:35 +0100

    exercicentri

--- AlgorithmesEncore/exercice5.js
+++ AlgorithmesEncore/exercice5.js
@@ -1,2 +1,18 @@
-const list1 = [3, 7, 8, 3, 6, 1];
-const list2 = [1, 4, 5, 8];
+const list1 = [10, 15, 3, 7];
+const n1 = 17;
+const list2 = [1, 8, 10, 21];
+const n2 = 19;
+function hasTwoNumbersWithSum(m, l) {
+  let numbersSeen = {};
+  for (let i = 0; i < m.length; i++) {
+    let otherNum = l - m[i];
+    if (numbersSeen[otherNum]) {
+      return true;
+    }
+    numbersSeen[m[i]] = true;
+  }
+  return false;
+}
+
+console.log(hasTwoNumbersWithSum(list1,n1));
+console.log(hasTwoNumbersWithSum(list2,n2))
